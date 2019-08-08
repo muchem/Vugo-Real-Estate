0bbedbea3549252987ec9f8115b6e4ee2d369a8b
muchemm <melissauchem@gmail.com>
2019-08-07 20:36:26 -0700

    add images to view

--- Controller.js
+++ Controller.js
@@ -1,6 +1,5 @@
 App.controller('main-controller',function($scope, listingFactory){
    $scope.data;
-   $scope.message = "hello";
    listingFactory.getListing().then(function(responce){
        $scope.data = responce.data;
    },function(error){
