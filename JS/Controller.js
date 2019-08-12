40d9d013e460171b183e932d75ffa1fde268e408
muchemm <melissauchem@gmail.com>
2019-08-11 20:06:09 -0700

    add price filter affect

--- JS/Controller.js
+++ JS/Controller.js
@@ -1,5 +1,9 @@
 App.controller('main-controller',function($scope, listingFactory){
    $scope.data;
+   $scope.price = {
+       min:100,
+       max:1000000
+   }
    listingFactory.getListing().then(function(responce){
        $scope.data = responce.data;
    },function(error){
