618e55652bab2ea61898178a90cedfc6cfc2d3cb
muchemm <melissauchem@gmail.com>
2019-08-30 19:02:42 -0700

    asjust layouts

---
+++ JS/filters.js
@@ -0,0 +1,13 @@
+App.filter('PriceFilter',function(){
+    return function(price,listing){
+        let output = [];
+        let max = price.max;
+        let min = price.min;
+        angular.forEach(listing, function (listing) {
+            if(listing.price <= max && listing.price >= min) {
+                output.push(listing);
+        }
+    });
+    return output;
+    }
+});
