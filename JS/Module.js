11935d279700a80dfc38daa59bd2841853e87eaa
muchemm <melissauchem@gmail.com>
2019-09-02 08:05:42 -0700

    add paginationbar directive

--- JS/Module.js
+++ JS/Module.js
@@ -152,4 +152,22 @@ App.directive('footerNav',function(){
 </footer>`
     }
 })
-
+App.directive('paginationBar',function(){
+    return{
+        template: `<nav aria-label="Page navigation"class = "pagination-bar">
+    <ul class="pagination pagination-md justify-content-center pag-bar">
+        <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+        </li>
+        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">4</a></li>
+        <li class="page-item"><a class="page-link" href="#">5</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+        </li>
+    </ul>
+</nav>`
+    }
+})
