5a827eecc9980be6ebed10f34040a71dbdc64cd6
muchemm <melissauchem@gmail.com>
2019-09-01 17:42:33 -0700

    remove price filter

--- JS/Controller.js
+++ JS/Controller.js
@@ -1,6 +1,5 @@
 App.controller('main-controller',function($scope, listingFactory,agentFactory){
    $scope.data;
-$scope.message = "Hello World";
    listingFactory.getListing().then(function(responce){
        $scope.data = responce.data;
    },function(error){
