f6b40ccb1fd89c32d19e13ea33796704b03e7028
muchemm <melissauchem@gmail.com>
2019-09-15 16:40:04 -0700

    add bedroom and bathroom input fields to UI

--- JS/filters.js
+++ JS/filters.js
@@ -1,11 +1,15 @@
 App.filter('SearchFilter',function(){
-    return function (listing, price, squareFt) {
+    return function (listing, price, squareFt,bedrooms,bathrooms) {
         let output = [];
         let min = price.min;
         let max = price.max;
         let sizes = squareFt;
+        let beds = bedrooms;
+        let baths = bathrooms;
         angular.forEach(listing, function (listing) {
-            if (listing.price <= max && listing.price >= min && listing.details.squareFt >= sizes) {
+            if (listing.price <= max && listing.price >= min 
+            && listing.details.squareFt >= sizes && listing.details.bedrooms >= beds
+            && listing.details.bathrooms >= baths) {
                 output.push(listing);
         }
 
